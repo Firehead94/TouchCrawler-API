b7bc67cd6fd82699ca89d899b76b507747272cdf
Justin Scott <justinjonscott@gmail.com>
2020-01-12 10:05:35 -0500

    Prep for token validation testing

--- main.py
+++ main.py
@@ -1,62 +1,19 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api
+from database.connection import db
+from google.oauth2 import id_token
+from google.auth.transport import requests
 from configparser import ConfigParser
-import sqlalchemy
 
-app = Flask(__name__)
-api = Api(app)
 credentialsFile = "credentials.ini"
 credentials = ConfigParser()
 credentials.read(credentialsFile)
 
-DB_USER = credentials.get('main', 'DB_USER')
-DB_PASS = credentials.get('main', 'DB_PASS')
-DB_NAME = credentials.get('main', 'DB_NAME')
-DB_CLOUD_CONN_NAME = credentials.get('main', 'DB_CONN_NAME')
+CLIENT_ID = credentials.get('main', 'WEBAPI_CLIENT')
 
+app = Flask(__name__)
+api = Api(app)
 
-# [START cloud_sql_mysql_sqlalchemy_create]
-# The SQLAlchemy engine will help manage interactions, including automatically
-# managing a pool of connections to your database
-db = sqlalchemy.create_engine(
-    # Equivalent URL:
-    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
-    sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=DB_USER,
-        password=DB_PASS,
-        database=DB_NAME,
-        query={"unix_socket": "/cloudsql/{}".format(DB_CLOUD_CONN_NAME)},
-    ),
-    # ... Specify additional properties here.
-    # [START_EXCLUDE]
-    # [START cloud_sql_mysql_sqlalchemy_limit]
-    # Pool size is the maximum number of permanent connections to keep.
-    pool_size=5,
-    # Temporarily exceeds the set pool_size if no connections are available.
-    max_overflow=2,
-    # The total number of concurrent connections for your application will be
-    # a total of pool_size and max_overflow.
-    # [END cloud_sql_mysql_sqlalchemy_limit]
-    # [START cloud_sql_mysql_sqlalchemy_backoff]
-    # SQLAlchemy automatically uses delays between failed connection attempts,
-    # but provides no arguments for configuration.
-    # [END cloud_sql_mysql_sqlalchemy_backoff]
-    # [START cloud_sql_mysql_sqlalchemy_timeout]
-    # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-    # new connection from the pool. After the specified amount of time, an
-    # exception will be thrown.
-    pool_timeout=30,  # 30 seconds
-    # [END cloud_sql_mysql_sqlalchemy_timeout]
-    # [START cloud_sql_mysql_sqlalchemy_lifetime]
-    # 'pool_recycle' is the maximum number of seconds a connection can persist.
-    # Connections that live longer than the specified amount of time will be
-    # reestablished
-    pool_recycle=1800,  # 30 minutes
-    # [END cloud_sql_mysql_sqlalchemy_lifetime]
-    # [END_EXCLUDE]
-)
-# [END cloud_sql_mysql_sqlalchemy_create]
 
 class Test(Resource):
     def get(self):
@@ -73,6 +30,21 @@ class DBTest(Resource):
 
         return {"DB_Connection":DBconnection}
 
+class Validate(Resource):
+    def post(self, token):
+        try:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise ValueError('Invalid Token Issuer...')
+
+            userid = idinfo['sub']
+            print(f"userid = {userid}")
+        except ValueError:
+            pass
+
+
+
+api.add_resource(Validate, "/validate?=")
 api.add_resource(Test, '/')
 api.add_resource(DBTest, "/DB")
