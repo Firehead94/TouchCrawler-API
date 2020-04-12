fd396f19d025211615eb49d30ac2f3dc08991680
Justin Scott <justinjonscott@gmail.com>
2020-04-12 09:54:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class AddScore(Resource):
             top_Score_ref = db.collection(u'topscores').document(str(idinfo['sub']) + str(time.time()))
             try:
                 doc_ref.set({u'username':idinfo['name']})
-                doc_ref.set({u'scores': {str(datetime.datetime.now()):str(score)}}, merge=True)
+                doc_ref.update({u'scores': {str(datetime.datetime.now()):str(score)}}, merge=True)
 
                 top_Score_ref.set({u'date':datetime.datetime.now(),u'score':score,u'uid':idinfo['name']})
                 request = RequestBuilder({"success":True}, None)
